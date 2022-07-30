7cb297c6c0adae0ed71ceff87ad8d0fd3cc5dd26
olegnat <natsevsky@gmail.com>
2022-07-30 13:09:39 +0200

    Add feeForwarder logic to the vault

---
+++ pkg/vault/contracts/ForwardableProtocolFeesCollector.sol
@@ -0,0 +1,42 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+pragma solidity ^0.7.0;
+pragma experimental ABIEncoderV2;
+
+import "@balancer-labs/v2-interfaces/contracts/vault/IForwardableProtocolFeesCollector.sol";
+import "./ProtocolFeesCollector.sol";
+
+/**
+ * @dev This an auxiliary contract to the Vault, deployed by it during construction. It offloads some of the tasks the
+ * Vault performs to reduce its overall bytecode size.
+ *
+ * The current values for all protocol fee percentages are stored here, and any tokens charged as protocol fees are
+ * sent to this contract, where they may be withdrawn by authorized entities. All authorization tasks are delegated
+ * to the Vault's own authorizer.
+ */
+contract ForwardableProtocolFeesCollector is IForwardableProtocolFeesCollector, ProtocolFeesCollector {
+
+    address public immutable feeForwarder;
+
+    constructor(IVault _vault, address _feeForwarder) ProtocolFeesCollector(_vault){
+        require(address(_feeForwarder) != address(0), "FeeForwarder should be specified");
+        feeForwarder = _feeForwarder;
+    }
+
+    function getFeeDestination() external view override returns (address) {
+        return feeForwarder;
+    }
+
+}
