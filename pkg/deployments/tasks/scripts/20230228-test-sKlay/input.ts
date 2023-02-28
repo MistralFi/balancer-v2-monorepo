cb038a5f7b7ae791e88ecae96697607dbc986b1b
AlehNat <natsevsky@gmail.com>
2023-02-28 23:53:53 +0100

    add test sKlay token

---
+++ pkg/deployments/tasks/scripts/20230228-test-sKlay/input.ts
@@ -0,0 +1,13 @@
+export type TestTokenDeployment = {
+  name: string;
+  symbol: string;
+  decimals: number;
+};
+
+export default {
+  baobab: {
+    name: 'testSKlay',
+    symbol: 'tSKLAY',
+    decimals: 18,
+  },
+};
